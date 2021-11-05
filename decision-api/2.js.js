1b73348e20153ba902c82e16582308b49668ea16
Merveille KITOKO <merkitoko@gmail.com>
2021-11-05 21:48:39 +0100

    update js code sample

--- decision-api/2.js.js
+++ decision-api/2.js.js
@@ -1,19 +1,20 @@
-fetch("https://decision.flagship.io/v2/{{ENV_ID}}/campaigns", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': '{{API_KEY}}'
-        },
-        body: JSON.stringify({
-          visitor_id: "YOUR_VISITOR_ID",
-          context: {
-              
-          },
-          // For the Decision API to trigger a campaign activation hit, use
-          trigger_hit: true,
-          // Optional : see https://developers.flagship.io/docs/decision-api/v2#decision-group for more details
-          decision_group: null
-        })
-  })
-.then(response => response.json())
-.then(data => console.log(data));
+//  Please follow the documentation in order to install and implement the Flagship JS SDK
+//  https://developers.flagship.io/docs/sdk/javascript/v2.2
+
+import Flagship from "@flagship.io/js-sdk";
+
+const fsInstance = Flagship.start("{{ENV_ID}}", "{{API_KEY}}");
+
+const fsVisitor = fsInstance.newVisitor({
+  visitorId: "YOUR_VISITOR_ID",
+  context: {
+    // some context
+  },
+});
+
+fsVisitor?.on("ready", (error) => {
+  if (error) {
+    console.log("error:", error);
+  }
+  // Sdk ready
+});
