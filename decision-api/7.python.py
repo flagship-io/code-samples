34150e00fa786d5e93d22b255eace444f9a890cf
raf-abtasty <raphael@abtasty.com>
2023-01-25 12:16:08 +0100

    Update 7.python.py
    Add imports & quotes

--- decision-api/7.python.py
+++ decision-api/7.python.py
@@ -1,7 +1,11 @@
 #  Please follow the documentation in order to install and implement the Flagship Python SDK
 #  https://developers.flagship.io/docs/sdk/python/v2.1
 
-Flagship.instance().start({{ENV_ID}}, {{API_KEY}}, Config(mode=Config.Mode.API))
+from flagship.app import Flagship
+from flagship.config import Config
+
+
+Flagship.instance().start("{{ENV_ID}}", "{{API_KEY}}", Config(mode=Config.Mode.API))
 context = {
     'try':'me'
 }
